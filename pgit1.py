45940307c839dc0ca62f874e57eafcf286c5595e
balraj <gbalraj2000@gmail.com>
2020-12-13 19:01:01 +0530

    my first change

--- pgit1.py
+++ pgit1.py
@@ -1,1 +1,10 @@
-print("balraj just started")
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=== RESTART: C:/Users/Balraj/AppData/Local/Programs/Python/Python38/pgit1.py ===
+balraj just started
+>>> 
+=============== RESTART: C:/Users/Balraj/Desktop/gitsub/pgit1.py ===============
+shiva shankar got irritateed so he went for a new file
+oh my god
+>>> 
