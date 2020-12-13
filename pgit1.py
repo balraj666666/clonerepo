ca3be64e4df90948e4fad49da7244703bbfc45b0
balraj <gbalraj2000@gmail.com>
2020-12-13 20:10:15 +0530

    checking the reverting of committed

--- pgit1.py
+++ pgit1.py
@@ -1,10 +1,10 @@
-Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-=== RESTART: C:/Users/Balraj/AppData/Local/Programs/Python/Python38/pgit1.py ===
-balraj just started
->>> 
-=============== RESTART: C:/Users/Balraj/Desktop/gitsub/pgit1.py ===============
-shiva shankar got irritateed so he went for a new file
-oh my god
->>> 
+print("shiva shankar got irritateed so he went for a new file")
+print("oh my god")
+print("new change")
+c=input('enter the status')
+if(c=='yes'):
+    print("eat samosa")
+else:
+    print("not samosa")
+    print("eat goodies...")
+
